7827856296ed7f04c51d26ae53bd2f20382be4bc
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-12-02 11:34:38 +0700

    feat(add): adding heading for each hosting item

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,34 @@
 import 'bootstrap';
 
 import '../scss/index.scss';
+import hostingPrice from './hostingPrice';
 
 $('#alert').on('click', () => {
   alert('jQuery works!');
 });
 
-// Your jQuery code
+const [hostingPriceListContainer] = $('.hosting-price__list');
+function hostingPriceItemHTML(hosting) {
+  const element = document.createElement('div');
+  element.classList.add('hosting-price__item');
+  element.innerHTML = `
+    <div class="hosting-price__item__heading">
+      <h4 class="hosting-price__item__name">${hosting.name}</h4>
+      <div class="hosting-price__item__price">
+        <div class="hosting-price__item__price__original">
+          <s>Rp${hosting.originalPrice}</s>
+        </div>
+        <div class="hosting-price__item__price__discount">
+          <span class="hosting-price__item__price__currency">Rp</span>
+          <span class="hosting-price__item__price__nominal">${hosting.discountPrice}</span>
+        </div>
+        <div>per bulan</div>
+      </div>
+    </div>
+  `;
+
+  return element;
+}
+hostingPrice.forEach(hosting => {
+  hostingPriceListContainer.appendChild(hostingPriceItemHTML(hosting));
+});
